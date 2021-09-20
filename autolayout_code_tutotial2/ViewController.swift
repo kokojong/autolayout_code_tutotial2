46e27632caaacf539b5f19d61f59d08dcd569595
kokojong <duwhd0349@kookmin.ac.kr>
2021-09-20 16:20:55 +0900

    Initial Commit

---
+++ autolayout_code_tutotial2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout_code_tutotial2
+//
+//  Created by kokojong on 2021/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
